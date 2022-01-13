501ab072f0c2b3eacb384d43655a474c27a2b00c
Grace <grace@primetrust.com>
2022-01-13 12:05:58 -0800

    🧽

--- lib/adequate_crypto_address/cryptos.rb
+++ lib/adequate_crypto_address/cryptos.rb
@@ -6,12 +6,10 @@ module AdequateCryptoAddress
 
     def initialize(address_string)
       @address = address_string
-      # binding.pry
     end
 
     def valid?(address)
       valid_prefix?
     end
-    # alias valid? valid_prefix?
   end
 end
